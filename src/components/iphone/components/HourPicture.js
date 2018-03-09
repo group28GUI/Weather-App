b284f9db58c57429d632026018b69abbf85b6004
Abubaker Osman <abubaker-osman@live.co.uk>
2018-03-09 12:13:08 +0000

    Update HourPicture.js
    COMMENTS for HourPicture!

--- src/components/iphone/components/HourPicture.js
+++ src/components/iphone/components/HourPicture.js
@@ -1,26 +1,24 @@
-import { h, render, Component } from 'preact';
-
+import {h, render, Component} from 'preact';
 import style from '../style';
 
-export class HourPicture extends Component
-{
-  constructor(props){
-		super(props);
-	}
+export class HourPicture extends Component {
+  // Constructor with initial set states!
+  constructor(props) {
+    super(props);
+  }
 
-  render()
-  {
+  // OUTPUT (Time, Icon & Temperature) for hourly forcast condition!
+  render() {
     const tempStyles = this.props.temp ? `${style.temperaturehourly} ${style.filledhourly}` : style.temperaturehourly;
-    if (this.props.temp){
+    if (this.props.temp) {
       return (
-        <div className={style.hourlyforcastday}>
+        <div className = {style.hourlyforcastday}>
             <div>{this.props.hour} : 00</div>
             <img src = {this.props.icon} alt = "string"/>
-            <div className={ tempStyles }>{this.props.temp}</div>
-        </div>
-      )
+            <div className = {tempStyles}>{this.props.temp}</div>
+        </div>);
     }
-    else
+    else {
       return <div></div>
-  }
-}
+    }}
+} // END class HourPicture!
