80d2c7e75dc95981aa52a5a4d1d80b31af5c0db4
Abubaker Osman <abubaker-osman@live.co.uk>
2018-03-09 12:47:59 +0000

    Update PictureText.js
    COMMENTS for PictureText

--- src/components/iphone/components/PictureText.js
+++ src/components/iphone/components/PictureText.js
@@ -1,22 +1,19 @@
-import { h, render, Component } from 'preact';
-
+import {h, render, Component} from 'preact';
 import style from '../style';
 
-export class PictureText extends Component
-{
-  // a constructor with initial set states
-	constructor(props){
-		super(props);
-	}
+export class PictureText extends Component {
+    // Constructor with initial set states!
+    constructor(props) {
+	super(props);
+    }
 
+  // OUTPUT (Chance of Rain, Wind Speed & Cloud Coverage) for hourly forcast condition!
   render() {
-    return (
-      <div>
-        <img src = {this.props.data.image} />
-        <div>{this.props.data.text}</div>
-				<div> {this.props.data.value} {this.props.data.measure}</div>
-      </div>
-    );
-  }
-
-}
+      return (
+	<div>
+           <img src = {this.props.data.image}/>
+           <div>{this.props.data.text}</div>
+	   <div>{this.props.data.value} {this.props.data.measure}</div>
+        </div>);
+   }
+} // END class PictureText!
