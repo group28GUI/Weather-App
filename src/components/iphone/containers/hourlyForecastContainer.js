09ae314cf637ebcfa6adb4b868e860d06522e29d
Abubaker Osman <abubaker-osman@live.co.uk>
2018-03-09 14:55:05 +0000

    Update hourlyForecastContainer.js
    COMMENTS for hourlyForecastContainer

--- src/components/iphone/containers/hourlyForecastContainer.js
+++ src/components/iphone/containers/hourlyForecastContainer.js
@@ -1,28 +1,21 @@
-// import preact
-import { h, render, Component } from 'preact';
-
+import {h, render, Component} from 'preact';
+import style from '../style';
 import {HourPicture} from '../components/HourPicture';
 
-import style from '../style';
+export class HourlyForecastContainer extends Component {
+  constructor(props) {
+     super(props);
+  }
 
-export class HourlyForecastContainer extends Component{
-  constructor(props){
-		super(props);
-	}
-// this returns the hour time, icon image and the temperture which will be displayed on the hourly box.
+  // DATA for each row (Day, Temperature, Chance of Rain & Cloud Coverage)!
   returnEachRow = (data) => {
-    return (
-        <HourPicture temp = {data.temp} hour = {data.hour} icon = {data.icon}/>
-    );
+     return (<HourPicture temp ={data.temp} hour ={data.hour} icon ={data.icon}/>);
   }
 
-  render()
-  {
-     // split the data on each day
+  // OUTPUT data for each row!
+  render() {
      const content = this.props.hourly.map((data) => this.returnEachRow(data));
-     return(
-      <div class={style.hourlyforcast}>
-        {content}
-      </div>);
-  }
-}
+     return (
+        <div class = {style.hourlyforcast}>{content}</div>
+  );}
+} // END class hourlyForecastContainer!
